c2f756230be87540cd4c76c9b0f721af24bd6089
JaeHyung Jang <jaehyung.jang@rgpkorea.co.kr>
2016-10-05 19:02:35 +0900

    Keep PEP8

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, patterns, include
-from lists import views
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^$', 'lists.views.home_page', name='home'),
     url(r'^lists/', include('lists.urls')),
 )
