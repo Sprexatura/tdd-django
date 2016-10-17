fb19ae2cd8059a690894dc8a0af50596d9bf3495
JaeHyung Jang <jaehyung.jang@rgpkorea.co.kr>
2016-10-17 19:48:59 +0900

    Improving layout: Bootstrap

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisterTest(LiveServerTestCase):
+class NewVisterTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(3)
