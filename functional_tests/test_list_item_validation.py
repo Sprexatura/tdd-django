ecbe6da8b9420701005ae9202ff7e57aaec1f1a6
JaeHyung Jang <jaehyung.jang@rgpkorea.co.kr>
2016-12-28 18:34:29 +0900

    NOT WORKED. change item id and name

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -7,18 +7,18 @@ class ItemValidationTest(FunctionalTest):
         # 에디스가 메인 페이지에서 빈 아이템을 실수로 등록하려고 함
         # 빈 입력상자에서 엔터를 입력
         self.browser.get(self.server_url)
-        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        self.get_item_input_box().send_keys('\n')
 
         # 페이지가 새로고침되고 빈 아이템 등록 거부 메세지 표시
         error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # 다른거 입력하면 잘 입력됨
-        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+        self.get_item_input_box().send_keys('Buy milk\n')
         self.check_for_row_in_list_table('1: Buy milk')
 
         # 다시 빈거를 입력
-        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        self.get_item_input_box().send_keys('\n')
 
         # 다시 에러 메세지 표시
         self.check_for_row_in_list_table('1: Buy milk')
@@ -26,6 +26,6 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # 정상입력하면 잘 동작
-        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+        self.get_item_input_box().send_keys('Make tea\n')
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')
--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -12,10 +12,10 @@ class NewVisterTest(FunctionalTest):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
+            'Enter the item'
         )
 
         inputbox.send_keys('Buy peacock feathers')
@@ -27,7 +27,7 @@ class NewVisterTest(FunctionalTest):
 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
@@ -43,7 +43,7 @@ class NewVisterTest(FunctionalTest):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
 
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
 
