31a4244cc876b8a3b844808f8c343664ac26506c
JaeHyung Jang <jaehyung.jang@rgpkorea.co.kr>
2017-01-16 19:07:19 +0900

    Page 255 done

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -29,3 +29,17 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys('Make tea\n')
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')
+
+    def test_cannot_add_duplicate_items(self):
+        # 에디스는 메인 페이지로 돌아가서 신규 목록을 시작
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('Buy Coke\n')
+        self.check_for_row_in_list_table('1: Buy Coke')
+
+        # 같은 아이템을 입력
+        self.get_item_input_box().send_keys('Buy Coke\n')
+
+        # Error Msg Print
+        self.check_for_row_in_list_table('1: Buy Coke')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, 'Duplicate Item')
